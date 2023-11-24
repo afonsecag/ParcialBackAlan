bd26a26bccb9680427e3f76d40200e3002d94445
Alan Snade Fonseca Garcia <a.fonsecag@uniandes.edu.co>
2023-11-24 09:13:31 -0500

    "Primera parte terminada"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { PerformerModule } from './performer/performer.module';
+import { AlbumPerformerModule } from './album-performer/album-performer.module';
+import { AlbumEntity } from './album/album.entity';
+import { TrackEntity } from './track/track.entity';
+import { PerformerEntity } from './performer/performer.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ AlbumModule, TrackModule, PerformerModule, AlbumPerformerModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'museum',
+    entities: [AlbumEntity, TrackEntity, PerformerEntity],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
